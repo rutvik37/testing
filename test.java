4f6e7fdc32f9d5e023508fd4eee906dc65e5e09c
Rutvik Jasani <rutvikjasani2705@gmail.com>
2025-06-22 06:40:26 +0530

    mmm

--- test.java
+++ test.java
@@ -2,10 +2,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("hello world 1");
-        System.out.println("hello world 100");
-        
-
-        System.out.println("new branch commit");
+   
 
     }
 }
