a0e163948c0fe5529384cd0790886d5ab047441f
Rutvik Jasani <rutvikjasani2705@gmail.com>
2025-06-22 06:09:38 +0530

    nn

--- test.java
+++ test.java
@@ -3,15 +3,5 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world 1");
 
-        System.out.println("hello world 2");
-
-        System.out.println("hello world 5");
-
-        System.out.println("hello world 6");
-
-        System.out.println("hello world 7");
-
-        System.out.println("hello world 8");
-
     }
 }
