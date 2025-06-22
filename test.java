3fb1bf311058e6c61b39e01e71f3b392a84a57bd
Rutvik Jasani <rutvikjasani2706@gmail.com>
2025-06-22 06:12:08 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("hello world 1");
+        System.out.println("hello world 100");
+        
 
     }
 }
